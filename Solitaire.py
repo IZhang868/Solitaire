3ea54b019cfc8571150e4347ee0ad8937b21dc79
Ian <ianzhang8168@gmail.com>
2022-11-04 13:50:07 -0700

    yea

--- Solitaire.py
+++ Solitaire.py
@@ -1,4 +1,20 @@
 import random
 import math
 import pygame
-from pygame import *
+import sys
+from pygame.locals import *
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((800, 800))
+    while True:
+        for event in pygame.event.get():
+            print(event)
+            if event.type == QUIT:
+                pygame.quit
+                sys.exit
+                break
+        pygame.display.update()
+    GREEN = (0, 255, 0)
+    
+main()
