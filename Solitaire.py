5b6dd7799fe3f9c61c1ae70683cc22b1e77ba83d
Ian <ianzhang8168@gmail.com>
2022-11-07 00:01:16 -0800

    Pygame window works

--- Solitaire.py
+++ Solitaire.py
@@ -7,14 +7,15 @@ from pygame.locals import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((800, 800))
-    while True:
+    running = True
+    GREEN = (0, 255, 0)
+    screen.fill(GREEN)
+    pygame.display.flip()
+    while running == True:
         for event in pygame.event.get():
             print(event)
-            if event.type == QUIT:
-                pygame.quit
-                sys.exit
-                break
-        pygame.display.update()
-    GREEN = (0, 255, 0)
-    
+            if event.type == pygame.QUIT:
+                pygame.display.quit()
+                running == False
+            pygame.draw.line(200, 0)
 main()
