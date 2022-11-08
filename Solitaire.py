a056a047526e15966d0882eececd67743349aa99
Ian <ianzhang8168@gmail.com>
2022-11-08 13:51:55 -0800

    yes

--- Solitaire.py
+++ Solitaire.py
@@ -3,19 +3,25 @@ import math
 import pygame
 import sys
 from pygame.locals import *
-
+import PNG
 def main():
     pygame.init()
-    screen = pygame.display.set_mode((800, 800))
+    screen = pygame.display.set_mode((1600, 800))
     running = True
-    GREEN = (0, 255, 0)
-    screen.fill(GREEN)
+    Board_Color = (6, 124, 53)
+    BLACK = (0, 0, 0)
+    screen.fill(Board_Color)
     pygame.display.flip()
     while True:
         for event in pygame.event.get():
             print(event)
             if event.type == pygame.QUIT:
                 pygame.display.quit()
-                break
-            pygame.draw.line(200, 0)
+            elif event.type == MOUSEBUTTONUP:
+                print(pygame.mouse.get_pos())
+                x = pygame.mouse.get_pos
+            pygame.draw.line(screen, BLACK, (300,0), (300,400))
+            img = pygame.image.load('resized 9s.png')
+            screen.blit(img, (0,0))
+        pygame.display.flip()    
 main()
