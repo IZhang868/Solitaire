c9c2683c6aaddf7e4d352f38568c96471b16400e
Ian <ianzhang8168@gmail.com>
2022-11-10 10:24:55 -0800

    card randomizer created

--- Solitaire.py
+++ Solitaire.py
@@ -3,9 +3,23 @@ import math
 import pygame
 import sys
 from pygame.locals import *
-import PNG
 def card_random():
-def main():
+    #         0    1    2    3
+    suits = ['H', 'S', 'D', 'C']
+    #               0   1   2   3   4   5   6   7   8   9 indexing
+    card_number = ['1','2','3','4','5','6','7','8','9','10']
+    randomized_card_list = []
+    suits_random = random.randint(0,3)
+    card_random = random.randint(0,9)
+    print(card_random)
+    print(suits_random)
+    for i in range(0,52):
+        final_card = card_number[card_random] + suits[suits_random]
+        randomized_card_list.append(final_card)
+        
+    return(randomized_card_list)
+    
+#def main():
     pygame.init()
     screen = pygame.display.set_mode((1600, 800))
     running = True
@@ -25,4 +39,5 @@ def main():
             img = pygame.image.load('resized 9s.png')
             screen.blit(img, (0,0))
         pygame.display.flip()    
-main()
+#main()
+card_random()
